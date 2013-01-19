492453ecf6889237b2d0926341246aff0b9511eb
shengbinmeng <shengbinmeng@gmail.com>
2013-01-19 12:57:20 +0800

    Initial Commit

---
+++ ChiSha/ChiShaAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ChiShaAppDelegate.h
+//  ChiSha
+//
+//  Created by Shengbin Meng on 13-1-19.
+//  Copyright (c) 2013年 Peking University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChiShaAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ChiSha/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ChiSha
+//
+//  Created by Shengbin Meng on 13-1-19.
+//  Copyright (c) 2013年 Peking University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ChiShaAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ChiShaAppDelegate class]));
+    }
+}
