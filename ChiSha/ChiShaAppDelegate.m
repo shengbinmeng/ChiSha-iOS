8f694177843165d65fce893fad054e608ec9df65
Shengbin Meng <shengbinmeng@gmail.com>
2014-08-21 16:55:27 +0800

    save data in the right time

--- ChiSha/ChiShaAppDelegate.m
+++ ChiSha/ChiShaAppDelegate.m
@@ -34,6 +34,10 @@
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    if ([[[[ChiShaChoiceManager getChoiceList] objectAtIndex:0] valueForKey:@"name"] isEqualToString:@"添加新选项..."]) {
+        [[ChiShaChoiceManager getChoiceList] removeObjectAtIndex:0];
+    }
+    [ChiShaChoiceManager saveData];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -49,10 +53,6 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    if ([[[[ChiShaChoiceManager getChoiceList] objectAtIndex:0] valueForKey:@"name"] isEqualToString:@"添加新选项..."]) {
-        [[ChiShaChoiceManager getChoiceList] removeObjectAtIndex:0];
-    }
-    [ChiShaChoiceManager saveData];
 }
 
 @end
